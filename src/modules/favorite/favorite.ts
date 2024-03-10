46b5b1e0223747d24fe4b7b8a5361a9b3529c1a2
DonVladon96 <Petrov.Vladik2010@yandex.com>
2024-03-10 23:33:06 +0400

    fix bugs with likes

--- src/modules/favorite/favorite.ts
+++ src/modules/favorite/favorite.ts
@@ -1,12 +1,24 @@
 //создам новый компоннет favorite.ts
 import { Component } from '../component';
 import html from './favorite.tpl.html';
+import { ProductList } from '../productList/productList';
+import { favoriteService } from '../../services/favorite.service';
 
-class Favotite extends Component {
+class Favorite extends Component {
+  productList: ProductList;
+
+  constructor(props: any) {
+    super(props);
+
+    this.productList = new ProductList();
+    this.productList.attach(this.view.products);
+  }
 
   async render() {
+    const products = await favoriteService.get();
 
+    this.productList.update(products);
   }
 }
 
-export const favoriteComp = new Favotite(html);
+export const favoriteComp = new Favorite(html);
